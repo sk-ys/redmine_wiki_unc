261eddb7d0f266b8b4a081b6e3857d9d3493452d
sk-ys <963792+sk-ys@users.noreply.github.com>
2020-05-13 20:14:39 +0900

    fix when updating notes

--- assets/javascripts/redmine_wiki_unc_replace_path.js
+++ assets/javascripts/redmine_wiki_unc_replace_path.js
@@ -1,14 +1,21 @@
-$(function () {
-  var unc_to_file_proto = function(path_unc){
-      return 'file:///' + path_unc.replace(/"/g, '').replace(/\\/g, '/')
-  }
-  
-  $("span.path_unc").replaceWith(function() {
-      return $('<a/>', {
-        href: unc_to_file_proto(this.innerHTML),
-        html: this.getAttribute("name"),
-        class: "external"  // ignored?
-      });
-  });
+var replaceUncTag = function(){
+    var uncToFileProto = function(path_unc){
+        return 'file:///' + path_unc.replace(/"/g, '').replace(/\\/g, '/')
+    }
 
+    $("span.path_unc").replaceWith(function() {
+        return $('<a/>', {
+            href: uncToFileProto(this.innerHTML),
+            html: this.getAttribute("name"),
+            class: 'external'  // ignored
+        });
+    });
+}
+
+$(function () {
+    $(document).on('DOMSubtreeModified', 'div.wiki-preview', function() {
+        replaceUncTag();
+    });
+
+    replaceUncTag();
 });
