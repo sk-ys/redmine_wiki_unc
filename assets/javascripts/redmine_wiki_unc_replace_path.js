919d82ee984739f3dc679eb304aa9b7e4a852356
sk-ys <963792+sk-ys@users.noreply.github.com>
2021-02-18 07:47:12 +0900

    add button for copy to clipboard

--- assets/javascripts/redmine_wiki_unc_replace_path.js
+++ assets/javascripts/redmine_wiki_unc_replace_path.js
@@ -3,13 +3,34 @@ var formatUncTag = function(){
         return 'file:///' + path_unc.replace(/"/g, '').replace(/\\/g, '/')
     }
 
-    $("span.path_unc").replaceWith(function() {
-        return $('<a/>', {
-            href: uncToFileProto(this.innerHTML),
-            html: this.getAttribute("name"),
-            class: 'external'  // ignored
+    var enableClipboardJs = function () {
+        new ClipboardJS('.path_unc', {
+            text: function(trigger) {
+                return trigger.getAttribute('data-clipboard-text');
+            }
         });
+    }
+
+    $('span.path_unc').replaceWith(function() {
+        return $('<span/>', {class: 'path_unc_outer'}).append(
+            $('<a/>', {
+                href: uncToFileProto(this.innerHTML),
+                html: this.getAttribute('name'),
+                class: 'external',
+                target: '_blank'
+            })
+        ).append(
+            $('<a/>', {
+                html: '',
+                class: 'path_unc icon-only icon icon-copy',
+                'data-clipboard-text': this.innerHTML.replace(/"/g, ''),
+                style: 'cursor: pointer',
+                title: 'パスをコピーする'
+            })
+        );
     });
+
+    setTimeout(enableClipboardJs);
 }
 
 $(function () {
