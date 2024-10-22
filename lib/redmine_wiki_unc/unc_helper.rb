1acc69896df0232e0b0236dea1d1310b99d8167b
sk-ys <yskrmt@gmail.com>
2024-10-22 12:23:48 +0000

    Remove redmine_wiki_unc_replace_path.js

--- lib/redmine_wiki_unc/unc_helper.rb
+++ lib/redmine_wiki_unc/unc_helper.rb
@@ -11,7 +11,7 @@ module RedmineWikiUnc
     def unc_to_file_proto(str)
       Rails.logger.info "str == #{str}, is_unc? == #{is_unc?(str)}, head=#{@head.to_s}"
       return "" if !is_unc?(str)
-      str.gsub(@head, "file://///").gsub(/\\/, "/")
+      str.gsub(@head, "file://").gsub(/\\/, "/")
     end
 
     def trim(str)
@@ -29,8 +29,13 @@ module RedmineWikiUnc
     def get_tag(args)
       return "(No parameters are specified. A UNC path is needed at least.)" if args.empty?
       unc, label = parse_args(args)
-
-      return "<span class='path_unc' name=#{label}>".html_safe + "\"#{unc}\"" + "</span>".html_safe
+      html = <<~TEXT
+        <span class='path_unc_outer'>
+          <a href='#{unc_to_file_proto(unc)}' class='external' target='_blank'>#{label}</a>
+          <a class='path_unc icon-only icon icon-copy' data-clipboard-text='#{unc}' title='#{I18n.t(:label_copy_path)}' onclick='copyTextToClipboard(this)'></a>
+        </span>
+      TEXT
+      return html.html_safe
     end
   end
 end
