a44d4eb6c5ae29eea51a62a05875fde3a68c9802
sk-ys <yskrmt@gmail.com>
2024-10-25 08:23:23 +0900

    Remove unused script

--- lib/redmine_wiki_unc_hooks.rb
+++ lib/redmine_wiki_unc_hooks.rb
@@ -1,4 +1,3 @@
 class RedmineWikiUncHooks < Redmine::Hook::ViewListener
   render_on :view_layouts_base_html_head, :partial => "redmine_wiki_unc/redmine_wiki_unc_partial"
-  render_on :view_journals_update_js_bottom, :partial => "redmine_wiki_unc/update"
 end
